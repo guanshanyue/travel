7764ccb7d6e0a23a012539caeaeb8cd8bdfb6dd0
guanshanyue <1075922626@qq.com>
2018-07-16 21:49:56 +0800

    完成循环轮播功能

---
+++ travel/src/pages/home/component/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="swiper-wapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id" >
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true
+      },
+      swiperList: [
+        {
+          id:1,
+          imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1804/bd/8e4a1c3f470d3702.jpg_750x200_f1f0a8c7.jpg'
+        },
+        {
+          id:2,
+          imgUrl: 'https://imgs.qunarzz.com/piao/fusion/1807/c6/44fce1467be17702.jpg_750x200_406f5fc3.jpg'
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .swiper-wapper >>> .swiper-pagination-bullet-active
+    background: #ffffff
+  .swiper-wapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 26.66%
+    .swiper-img
+      width: 100%
+</style>
